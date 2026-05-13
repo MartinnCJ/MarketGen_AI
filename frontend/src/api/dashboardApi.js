83b09798b2ca6df19885f2c5ff968b3033e284ef
Martin Andrés Caamaño Jofre <martincj120@gmail.com>
2026-05-12 23:50:49 -0400

    Borrando archivos duplicados

--- frontend/src/api/dashboardApi.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const API_URL = "http://127.0.0.1:8000/api/dashboard";
-
-export const getDashboardData = async () => {
-  const response = await fetch("http://127.0.0.1:8000/api/dashboard");
-  const data = await response.json();
-  return data;
-};
